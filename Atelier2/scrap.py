72d97980c0140cc3240bb8086e621b08ca9c5e3a
Dieppois <dieppoisedouard@gmail.com>
2025-12-18 13:28:59 +0100

    Fin Atelier2 ajout calendar et Json.dump

--- Atelier2/scrap.py
+++ Atelier2/scrap.py
@@ -29,7 +29,7 @@ resultats = []
 
 for item in items:
     titre = item.select_one('.Card-title').get_text(strip=True)
-    date = item.select_one('.Card-label').get_text(strip=True)
+    date_text = item.select_one('.Card-label').get_text(strip=True)
     lien = item.get('href')
     if lien.startswith("/"):
         lien = BASE_URL + lien
@@ -41,18 +41,46 @@ for item in items:
     soup_link = BeautifulSoup(res_link.text, 'html.parser')
     show_event = soup_link.select_one('div.SitIntro-container')
 
+
     infos_list = []
 
+    # Récupère les informations supplémentaires sous forme de liste
     if show_event is not None:
         list_info = show_event.select('ul > li')
-        infos_list = [li.get_text(strip=True) for li in list_info]        
+        infos_list = [li.get_text(strip=True) for li in list_info]
+      
+        
+    calendar = {
+        "month": None, 
+        "days": []
+        }
+
+    # ---------------------------
+    # Scraping de la page calendrier
+    # ---------------------------
+    calendar_container = soup_link.select_one('.SitCalendar-container')
+    calendar_item = calendar_container.select_one('.SitCalendar-item')
+
+    # Récupération du mois
+    calendar["month"] = calendar_item.select_one('.SitCalendar-month').text.strip()
+
+    # Récupération de tous les jours
+    for day_el in calendar_item.select('.SitCalendar-day'):
+        spans = day_el.find_all('span')
+        calendar["days"].append({
+            "day": spans[0].text.strip(),
+            "time": spans[1].text.strip()
+        })
+
 
     resultats.append({
         "titre": titre,
-        "date": date,
+        "date_text": date_text,
         "lien": lien,
-        "infos": infos_list
+        "infos": infos_list,
+        "calendar": calendar
     })
+    
     
 # ---------------------------
 # Sauvegarde
